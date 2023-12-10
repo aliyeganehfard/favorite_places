a4d28ed6a84739610150a0fe7b918514e06af831
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2023-12-10 18:57:25 +0330

    Initial commit

---
+++ lib/providers/user_places.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:favorite_places/models/place.dart';
+
+class UserPlaceNotifier extends StateNotifier<List<Place>> {
+  UserPlaceNotifier() : super(const []);
+
+  void addPlace(String title, File image) {
+    final newPlace = Place(title: title, image: image);
+    state = [newPlace, ...state];
+  }
+}
+
+final userPlacesProvider = StateNotifierProvider<UserPlaceNotifier,List<Place>>((ref) =>
+    UserPlaceNotifier());
